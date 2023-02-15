c09a7cf14cdd5c556a50a3ba3077d82c5cbde6c2
Erik Mendoza <efernandomendoza@gmail.com>
2023-02-15 12:41:51 -0600

    Adding logout button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,27 @@
+import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-export default function Login () {
-    const {
-        isAuthenticated,
-        loginWithRedirect,
-      } = useAuth0();
-    
-      return !isAuthenticated && (
-        <div>
-            <button onClick={loginWithRedirect}>Log in</button>
-        </div>
-        
-      );
+
+function LoginButton() {
+  const {
+    isAuthenticated,
+    loginWithRedirect,
+    logout
+  } = useAuth0();
+
+  if (!isAuthenticated) {
+    return <button onClick={loginWithRedirect}>Log in</button>
+ }
+ else {
+    return isAuthenticated && (<div>
+      <button onClick={() => logout({
+          logoutParams: {
+            returnTo: window.location.origin
+          }
+        })}>Logout</button>
+    </div>);
+  };
+
+
 }
+
+export default LoginButton;
